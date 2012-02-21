98709ec278f75484846f0e9a0845075834e2e2ea
Neville Li <neville@spotify.com>
2012-02-21 14:57:49 -0500

    make bootstrap script generic

---
+++ .dotfiles/scripts/bootstrap-dotfiles.sh
@@ -0,0 +1,47 @@
+#/bin/bash
+
+# dependencies:
+# git - obviously
+# vim-nox - vim with python and ruby support
+# ctags - for vim Tagbar
+# flake8 - for vim-flake8
+
+[[ -f /usr/bin/lsb_release ]] && DISTRO=$(lsb_release --codename --short)
+
+if [[ "${DISTRO}" == "squeeze" ]]; then
+    APTITUDE="aptitude -t squeeze-backports"
+elif [[ "${DISTRO}" == "oneiric" ]]; then
+    APTITUDE="aptitude"
+else
+    echo "Unsupported distribution: ${DISTRO}"
+    exit 1
+fi
+
+sudo ${APTITUDE} install git vim-nox ctags python-pip
+sudo pip install flake8
+
+# set up git repository
+cwd=$(pwd)
+cd ${HOME}
+git init
+git remote add origin git@github.com:nevillelyh/dotfiles.git
+git fetch
+git branch --set-upstream master origin/master
+git checkout
+git submodule update --init --recursive
+
+# patch vim
+sudo patch -p1 $(dpkg-query -L vim-runtime|grep filetype.vim) < .dotfiles/vim/patches/filetype-augroup.patch
+
+# change default shell
+chsh -s /bin/zsh
+
+# custom fonts for vim-powerline
+if [[ "${DISTRO}" == "oneiric" ]]; then
+    mkdir -p .fonts
+    cd .fonts
+    git clone https://github.com/scotu/ubuntu-mono-powerline.git
+    cd ..
+fi
+
+cd ${cwd}

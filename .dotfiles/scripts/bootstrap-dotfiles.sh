a42bd6ef893cdba75480686de276705b018fa1ec
Neville Li <neville@spotify.com>
2012-09-08 01:20:12 -0400

    refactor bootstrap script
    Change-Id: Ifa21801a95a61dbedc72ca8594e47df0590a44a9

--- .dotfiles/scripts/bootstrap-dotfiles.sh
+++ .dotfiles/scripts/bootstrap-dotfiles.sh
@@ -17,77 +17,99 @@
 # ipython, virtualenv
 # flake8 - for vim-flake8
 
-die () {
+BREWS="autojump ctags git macvim python tmux"
+DEB_PKGS="autojump build-essential curl exuberant-ctags git ruby-dev tmux vim-nox zsh"
+PIP_PKGS="ipython virtualenv flake8"
+
+die() {
     echo "Error: $1"
     exit 1
 }
 
-DEB_PKGS="build-essential curl exuberant-ctags git ruby-dev tmux vim-nox zsh"
-PIP_PKGS="ipython virtualenv flake8"
+aptitude() {
+    DISTRO=$(lsb_release --codename --short)
+    case ${DISTRO} in
+        squeeze)
+            APTITUDE="aptitude -t squeeze-backports"
+            SUDO="sudo"
+            ;;
+        precise)
+            APTITUDE="aptitude"
 
-# detect distribution
-[[ -f /usr/bin/lsb_release ]] || die "not a LSB system"
-DISTRO=$(lsb_release --codename --short)
-case ${DISTRO} in
-    squeeze)
-        APTITUDE="aptitude -t squeeze-backports"
-        SUDO="sudo"
-        ;;
-    precise)
-        APTITUDE="aptitude"
+            # personal system, make /usr/local personal and bypass sudo
+            SUDO=""
+            sudo mv /usr/local /usr/local.orig
+            sudo mkdir /usr/local
+            sudo chown $(whoami):$(groups | awk '{print $1}') /usr/local
+            ;;
+        *)
+            die "unsupported distribution: ${DISTRO}"
+            ;;
+    esac
+    sudo ${APTITUDE} install ${DEB_PKGS}
 
-        # personal system, make /usr/local personal and bypass sudo
-        SUDO=""
-        sudo mv /usr/local /usr/local.orig
-        sudo mkdir /usr/local
-        sudo chown $(whoami):$(groups | awk '{print $1}') /usr/local
-        ;;
-    *)
-        die "unsupported distribution: ${DISTRO}"
-        ;;
-esac
+    # custom fonts for vim-powerline
+    if [[ "${DISTRO}" == "precise" ]]; then
+        mkdir -p .fonts
+        cd .fonts
+        git clone https://github.com/scotu/ubuntu-mono-powerline.git
+        cd ..
+    fi
+}
 
-# basic packages
-sudo ${APTITUDE} install ${DEB_PKGS}
+homebrew() {
+    # homebrew packages
+    ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+    brew install ${BREWS}
+}
 
-# PIP packages
-curl http://python-distribute.org/distribute_setup.py | ${SUDO} python
-curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | ${SUDO} python
-${SUDO} pip install ${PIP_PKGS}
+pip() {
+    # PIP packages
+    curl http://python-distribute.org/distribute_setup.py | ${SUDO} python
+    curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | ${SUDO} python
+    ${SUDO} pip install ${PIP_PKGS}
+}
 
-# set up git repository
-cwd=$(pwd)
-cd ${HOME}
-git init
-git config branch.master.rebase true
-git remote add origin git@github.com:nevillelyh/dotfiles.git
-git fetch
-git reset --hard origin/master
-git branch --set-upstream master origin/master
-git submodule update --init --recursive
+git() {
+    # set up git repository
+    cwd=$(pwd)
+    cd ${HOME}
+    git init
+    git config branch.master.rebase true
+    git remote add origin git@github.com:nevillelyh/dotfiles.git
+    git fetch
+    git reset --hard origin/master
+    git branch --set-upstream master origin/master
+    git submodule update --init --recursive
+}
 
-# Vundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qall
+zsh() {
+    # change default shell
+    chsh -s /bin/zsh
+}
 
-# Command-T
-for cmd in ruby ruby1.8; do
-    command -v ${cmd} > /dev/null && RUBY=${cmd} && break
-done
-cd ${HOME}/.vim/bundle/command-t/ruby/command-t
-${RUBY} extconf.rb
-make
-cd ${HOME}
+vundle() {
+    # Vundle
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    vim +BundleInstall +qall
+}
 
-# change default shell
-chsh -s /bin/zsh
+commandt() {
+    for cmd in ruby ruby1.8; do
+        command -v ${cmd} > /dev/null && RUBY=${cmd} && break
+    done
+    cd ${HOME}/.vim/bundle/command-t/ruby/command-t
+    ${RUBY} extconf.rb
+    make
+    cd ${HOME}
+}
 
-# custom fonts for vim-powerline
-if [[ "${DISTRO}" == "precise" ]]; then
-    mkdir -p .fonts
-    cd .fonts
-    git clone https://github.com/scotu/ubuntu-mono-powerline.git
-    cd ..
-fi
+[[ -f /usr/bin/lsb_release ]] && aptitude
+[[ "$(uname -s)" == "Darwin" ]] && brew
+pip
+git
+zsh
+vundle
+commandt
 
 cd ${cwd}

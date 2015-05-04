a89d65bef792eb576de465d6499c8bdf8d8d32d8
Neville Li <neville@spotify.com>
2015-05-04 19:37:09 -0400

    update xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -37,7 +37,6 @@ newManageHook = composeAll
     [ "Cssh"
     -- , "Spotify"
     , "Vlc"
-    , "VirtualBox"
     ]
 
 defaultKeys = keys defaultConfig
@@ -61,15 +60,15 @@ toAddKeys XConfig{modMask = modm} =
     , ((modm .|. shiftMask, xK_s),        spawn screenCmd)
     , ((modm .|. shiftMask, xK_Return),   spawn termCmd)
     , ((modm,               xK_KP_Enter), windows W.swapMaster)
-    , ((modm,               xK_F10),      spawn spPrevCmd)
-    , ((modm,               xK_F11),      spawn spPlayCmd)
-    , ((modm,               xK_F12),      spawn spNextCmd)
+    -- , ((modm,               xK_F10),      spawn spPrevCmd)
+    -- , ((modm,               xK_F11),      spawn spPlayCmd)
+    -- , ((modm,               xK_F12),      spawn spNextCmd)
     -- , ((modm .|. shiftMask, xK_d),        spawn "setxkbmap -layout dvorak")
     -- , ((modm .|. shiftMask, xK_q),        spawn "setxkbmap -layout us")
     ]
-    ++
+    -- ++
     -- flip screen #1 and #2
-    [ ((modm .|. mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-    | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,0,2]
-    , (f, mask) <- [(W.view, 0), (W.shift, shiftMask)]
-    ]
+    -- [ ((modm .|. mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    -- | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,0,2]
+    -- , (f, mask) <- [(W.view, 0), (W.shift, shiftMask)]
+    -- ]

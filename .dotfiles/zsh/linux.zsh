5f407a1ab63d7ddd7d067fcf9003b0709219afb1
Neville Li <neville@spotify.com>
2018-01-08 21:02:30 -0500

    clean up

--- .dotfiles/zsh/linux.zsh
+++ .dotfiles/zsh/linux.zsh
@@ -1,3 +1,8 @@
+[[ -d /usr/lib/jvm/java-8-oracle ]] && export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+
 for pkg in $(find /opt -maxdepth 2 -name latest -type l); do
     [[ -d "${pkg}/bin" ]] && export PATH="${pkg}/bin:$PATH"
 done
+
+[[ -f /usr/bin/xdg-open ]] && alias open='xdg-open'
+[[ -f /usr/bin/ack-grep ]] && alias ack='ack-grep'
--- .dotfiles/zsh/osx.zsh
+++ .dotfiles/zsh/osx.zsh
@@ -1,8 +1,8 @@
+/usr/libexec/java_home -v 1.8 > /dev/null 2>&1 && export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+
 export PATH="/usr/local/bin:$PATH"
 [[ -d /usr/local/opt/python/libexec/bin ]] && export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
 pidof() {
     ps axc 2>/dev/null | awk "{if (\$5==\"$1\") print \$1}"
 }
-
-/usr/libexec/java_home -v 1.8 > /dev/null 2>&1 && export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)

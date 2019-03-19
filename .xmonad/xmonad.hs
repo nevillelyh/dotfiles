08cf8eb10dcee59151f2ad8e918b75852ac21c7e
Neville Li <neville@spotify.com>
2019-03-19 11:59:06 -0400

    make ThreeColMid default layout

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -22,9 +22,9 @@ main = xmonad $ gnomeConfig
 
 newLayout = smartBorders ( avoidStruts (
     -- 2 columns, main on the left
-    (   (ResizableTall 1 (2/100) (2/3) [])  -- wide main
+    (   (ThreeColMid 1 (2/100) (1/2))
+    ||| (ResizableTall 1 (2/100) (2/3) [])  -- wide main
     ||| (ResizableTall 1 (2/100) (1/2) [])    -- equal width
-    ||| (ThreeColMid 1 (2/100) (1/2))
     -- 2 columns, main on the right
     -- (   (reflectHoriz $ ResizableTall 1 (2/100) (2/3) [])  -- wide main
     -- ||| (reflectHoriz $ ResizableTall 1 (2/100) (1/2) [])    -- equal width

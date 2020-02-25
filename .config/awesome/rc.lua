5605b14d76d74c29fd34bd2d11f018498f30ec25
Neville Li <neville.lyh@gmail.com>
2020-02-24 20:09:14 -0500

    [awesome] use mod-shift-n for restore

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -348,7 +348,7 @@ globalkeys = gears.table.join(
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1)                end,
               {description = "select previous", group = "layout"}),
 
-    awful.key({ modkey, "Control" }, "n",
+    awful.key({ modkey, "Shift"   }, "n",
               function ()
                   local c = awful.client.restore()
                   -- Focus restored client

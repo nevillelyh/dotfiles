0f94ce788496b24c4662a24227a66ed78aeeaa56
Neville Li <neville.lyh@gmail.com>
2020-02-24 19:57:36 -0500

    [awesome] use mod-control-q for quit

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -328,7 +328,7 @@ globalkeys = gears.table.join(
               {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
-    awful.key({ modkey, "Shift"   }, "q", awesome.quit,
+    awful.key({ modkey, "Control" }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,

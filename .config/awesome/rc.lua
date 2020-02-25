ff3fe06242a153696a5cfd4a8fcd132628c0899a
Neville Li <neville.lyh@gmail.com>
2020-02-24 19:38:49 -0500

    [awesome] use mod-shift-enter for new terminal

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -324,7 +324,7 @@ globalkeys = gears.table.join(
         {description = "go back", group = "client"}),
 
     -- Standard program
-    awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
+    awful.key({ modkey, "Shift"   }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),

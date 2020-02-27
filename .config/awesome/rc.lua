99178b6a8dcf5fce7522b5714fbe779a51c4cf32
Neville Li <neville.lyh@gmail.com>
2020-02-26 21:57:33 -0500

    [awesome] shorten textclock widget

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -131,7 +131,7 @@ mykeyboardlayout = awful.widget.keyboardlayout()
 
 -- {{{ Wibar
 -- Create a textclock widget
-mytextclock = wibox.widget.textclock()
+mytextclock = wibox.widget.textclock("%a %H:%M")
 
 -- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(

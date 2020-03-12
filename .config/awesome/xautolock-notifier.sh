bcc7202f22c8db6718f7f2d4579829c3e4a9e564
Neville Li <neville.lyh@gmail.com>
2020-03-12 11:20:27 -0400

    [awesome] remove echo from xautolock-notifier.sh

--- .config/awesome/xautolock-notifier.sh
+++ .config/awesome/xautolock-notifier.sh
@@ -6,7 +6,6 @@ trap "xcalib -clear" EXIT
 # Fade to black in 5 seconds in 2%, 100ms increments
 for (( i = 1; i <= 50; i++ )); do
   let contrast=100-$i*2
-  echo $contrast
   xcalib -alter -contrast $contrast
   sleep 0.1
 done

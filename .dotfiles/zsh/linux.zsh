89eae250a4146578afabe4e40a4ca3a4d29be909
Neville Li <neville.lyh@gmail.com>
2020-03-20 22:08:36 -0400

    [vim] de-alias fdfind for nvim

--- .dotfiles/zsh/linux.zsh
+++ .dotfiles/zsh/linux.zsh
@@ -1,4 +1,9 @@
 alias open='xdg-open'
 
+FD_COMMAND=fd
 # fd is already used by package fdclone on Ubuntu 19.04 or later
-type fdfind &>/dev/null && alias fd='fdfind'
+if type fdfind >/dev/null 2>&1; then
+    alias fd='fdfind'
+    FD_COMMAND=fdfind
+fi
+export FD_COMMAND
--- .dotfiles/zsh/mac.zsh
+++ .dotfiles/zsh/mac.zsh
@@ -5,3 +5,5 @@ alias gvim='vimr'
 pidof() {
     ps axc 2>/dev/null | awk "{if (\$5==\"$1\") print \$1}"
 }
+
+export FD_COMMAND=fd

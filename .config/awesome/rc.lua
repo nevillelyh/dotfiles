4fc61227e788ff645ebebc312918a3249c036d8a
Neville Li <neville.lyh@gmail.com>
2020-04-14 19:53:51 -0400

    [awesome] disable fair layout

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -80,7 +80,7 @@ awful.layout.layouts = {
     awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
+    -- awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,

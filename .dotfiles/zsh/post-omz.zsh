23ae033e379b8622ca88b98f6d0814bd588559b7
Neville Li <neville.lyh@gmail.com>
2020-08-10 22:50:04 -0400

    [zsh] deduplicate $PATH

--- .dotfiles/zsh/post-omz.zsh
+++ .dotfiles/zsh/post-omz.zsh
@@ -47,3 +47,7 @@ source "$HOME/.cargo/env"
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+
+# Deduplicate $PATH
+typeset -aU path

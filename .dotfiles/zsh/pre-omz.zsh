3289210d5f6e07db6140b4b1d7a3227301992013
Neville Li <neville.lyh@gmail.com>
2021-01-04 17:50:54 -0500

    [zsh] add /opt/homebrew to PATH

--- .dotfiles/zsh/pre-omz.zsh
+++ .dotfiles/zsh/pre-omz.zsh
@@ -1,4 +1,5 @@
 # before oh-my-zsh
+[[ -d /opt/homebrew/bin ]] && export PATH=/opt/homebrew/bin:$PATH
 [[ -d $HOME/.local/bin ]] && export PATH=$HOME/.local/bin:$PATH
 [[ -d $HOME/go/bin ]] && export PATH=$HOME/go/bin:$PATH
 [[ -d /usr/local/go/bin ]] && export PATH=/usr/local/go/bin:$PATH

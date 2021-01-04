2d26485ff2dbda93a246d17fee5e47cff44cafef
Neville Li <neville.lyh@gmail.com>
2021-01-04 10:18:17 -0500

    [zsh] add go to PATH

--- .dotfiles/zsh/pre-omz.zsh
+++ .dotfiles/zsh/pre-omz.zsh
@@ -1,2 +1,4 @@
 # before oh-my-zsh
 [[ -d $HOME/.local/bin ]] && export PATH=$HOME/.local/bin:$PATH
+[[ -d $HOME/go/bin ]] && export PATH=$HOME/go/bin:$PATH
+[[ -d /usr/local/go/bin ]] && export PATH=/usr/local/go/bin:$PATH

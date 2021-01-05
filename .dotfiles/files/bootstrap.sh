9d0b6c3aa2a170af5ec9acd9add612226e8ed123
Neville Li <neville.lyh@gmail.com>
2021-01-05 12:01:59 -0500

    [bootstrap] add sublime-text

--- .dotfiles/files/bootstrap.sh
+++ .dotfiles/files/bootstrap.sh
@@ -78,9 +78,13 @@ _linux() {
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
     sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
     sudo apt-get install apt-transport-https
     sudo apt-get update
-    sudo apt-get install code # or code-insiders
+    sudo apt-get install code sublime-text # or code-insiders
+
+
 
     sudo snap install spotify
     sudo snap install hub --classic

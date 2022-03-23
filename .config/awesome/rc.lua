fc1445d2dfe17d3377fea8a2284fceac7a09e3ff
Neville Li <neville.lyh@gmail.com>
2022-03-23 14:04:32 -0400

    [awesome] disable title bar for Steam

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -860,6 +860,10 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
+
+    { rule_any = { class = { "Steam" },
+      }, properties = { titlebars_enabled = false }
+    },
 }
 -- }}}
 

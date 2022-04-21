fc66524f4918df98f384e1b7ce03a13286b49fa7
Neville Li <neville.lyh@gmail.com>
2022-04-21 00:02:39 -0400

    [zsh] remove go from PATH

--- .dotfiles/zsh/pre-omz.zsh
+++ .dotfiles/zsh/pre-omz.zsh
@@ -2,8 +2,6 @@
 
 [[ -d /opt/homebrew/bin ]] && export PATH=/opt/homebrew/bin:$PATH
 [[ -d $HOME/.local/bin ]] && export PATH=$HOME/.local/bin:$PATH
-[[ -d $HOME/go/bin ]] && export PATH=$HOME/go/bin:$PATH
-[[ -d /usr/local/go/bin ]] && export PATH=/usr/local/go/bin:$PATH
 
 # https://docs.brew.sh/Shell-Completion
 if type brew &>/dev/null

1275efb9904ede824b2de76c7a6646e647cd503f
Neville Li <neville.lyh@gmail.com>
2022-04-30 20:53:15 -0400

    [bootstrap] add build-essential

--- .dotfiles/files/bootstrap.sh
+++ .dotfiles/files/bootstrap.sh
@@ -18,7 +18,7 @@ CASKS_OPT="adobe-creative-cloud anki expressvpn firefox google-chrome guitar-pro
 # gnome-screensaver xautolock xcalib - for screen locking in awesome
 # unzip, zip - for SDKMAN
 # Not available or outdated in Ubuntu - bat, git-delta, zoxide
-DEB_PKGS="colordiff exa fd-find fzf htop neovim ripgrep snapd tmux unzip zip zsh"
+DEB_PKGS="build-essential colordiff exa fd-find fzf htop neovim ripgrep snapd tmux unzip zip zsh"
 DEB_GUI_PKGS="alacritty awesome compton fonts-powerline gnome-screensaver neovim-qt ubuntu-restricted-extras xautolock xcalib"
 LINUX_CRATES="bat git-delta gitui zoxide"
 

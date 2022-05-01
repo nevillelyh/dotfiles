3836f9bab3e5afc69c7da9a17d5f7fc900042f23
Neville Li <neville.lyh@gmail.com>
2022-05-01 13:35:20 -0400

    [bootstrap] fix Linux CMake setup

--- .dotfiles/files/bootstrap.sh
+++ .dotfiles/files/bootstrap.sh
@@ -82,6 +82,14 @@ setup_linux() {
 
     sudo snap install hub --classic
 
+    wget -qO - https://apt.kitware.com/keys/kitware-archive-latest.asc | gpg --dearmor > kitware-archive-keyring.gpg
+    sudo install -o root -g root -m 644 *.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [signed-by=/etc/apt/trusted.gpg.d/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main" > /etc/apt/sources.list.d/kitware.list'
+    rm -f *.gpg
+
+    sudo aptitude update
+    sudo aptitude install kitware-archive-keyring cmake
+
     # The following are GUI apps
     dpkg-query -l xorg &> /dev/null || return 0
 
@@ -91,17 +99,15 @@ setup_linux() {
     sudo dpkg -i google-chrome-stable_current_amd64.deb
     rm google-chrome-stable_current_amd64.deb
 
-    wget -qO - https://apt.kitware.com/keys/kitware-archive-latest.asc | gpg --dearmor > kitware-archive-keyring.gpg
     wget -qO - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor > sublimehq-pub.gpg
     sudo install -o root -g root -m 644 *.gpg /etc/apt/trusted.gpg.d/
-    sudo sh -c 'echo "deb [signed-by=/etc/apt/trusted.gpg.d/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main" > /etc/apt/sources.list.d/kitware.list'
     sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
     sudo sh -c 'echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list'
     rm -f *.gpg
 
     sudo aptitude update
-    sudo aptitude install kitware-archive-keyring cmake code sublime-text # or code-insiders
+    sudo aptitude install code sublime-text # or code-insiders
 
     git clone https://github.com/dracula/gnome-terminal
     ./gnome-terminal/install.sh

1b430d922261537579e6f51e3725f1aab46bfaa5
Neville Li <neville.lyh@gmail.com>
2022-05-01 20:49:22 -0400

    [bootstrap] use apt.sh for VS Code & Sublime

--- .dotfiles/files/bootstrap.sh
+++ .dotfiles/files/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # bash -c "$(curl -fsSL https://raw.github.com/nevillelyh/dotfiles/master/.dotfiles/files/bootstrap.sh)"
+APT_SH="https://raw.github.com/nevillelyh/dotfiles/master/.dotfiles/files/apt.sh"
 
 set -euo pipefail
 
@@ -88,15 +89,8 @@ setup_linux() {
     sudo dpkg -i google-chrome-stable_current_amd64.deb
     rm google-chrome-stable_current_amd64.deb
 
-    wget -qO - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor > sublimehq-pub.gpg
-    sudo install -o root -g root -m 644 *.gpg /etc/apt/trusted.gpg.d/
-    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-    sudo sh -c 'echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list'
-    rm -f *.gpg
-
-    sudo aptitude update
-    sudo aptitude install -y code sublime-text # or code-insiders
+    curl -fsSL "$APT_SH" | bash -s -- code
+    curl -fsSL "$APT_SH" | bash -s -- sublime
 
     git clone https://github.com/dracula/gnome-terminal
     ./gnome-terminal/install.sh

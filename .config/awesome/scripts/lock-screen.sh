1082c97de9a88aeba615ab4356fbd24d48a240be
Neville Li <neville.lyh@gmail.com>
2022-05-05 00:00:45 -0400

    [awesome] fix xautolock race condition

--- .config/awesome/scripts/lock-screen.sh
+++ .config/awesome/scripts/lock-screen.sh
@@ -8,8 +8,11 @@
 uri=$(gsettings get org.gnome.desktop.screensaver picture-uri)
 gsettings set org.gnome.desktop.background picture-uri $uri
 
-# Kill existing xautolock
+# Kill existing xautolock first
 xautolock -exit
+
+# Wait till the previous session exits
+sleep 3
 
 # Lock after 10 minutes with 5 seconds notifier
 xautolock -time 10 -locker "gnome-screensaver-command --lock" -notify 5 -notifier $HOME/.config/awesome/scripts/fade-out.sh

6b8a28164b056577b288c8555f37fb3c88897cca
Neville Li <neville.lyh@gmail.com>
2022-05-14 00:40:40 -0400

    [misc] support arm64 for joplin-cli

--- .dotfiles/files/joplin-cli/build.sh
+++ .dotfiles/files/joplin-cli/build.sh
@@ -6,11 +6,19 @@ set -euo pipefail
 
 image=nevillelyh/joplin-cli
 version=$(grep "npm install" Dockerfile | grep -o "\<joplin@[0-9\.]\+$" | cut -d "@" -f 2)
+arch=$(uname -m)
+case $arch in
+    aarch64)
+        arch=arm64
+        ;;
+    x86_64)
+        arch=amd64
+        ;;
+esac
 
 [[ -d dein.vim ]] && rm -rf dein.vim
 git clone https://github.com/Shougo/dein.vim.git
-docker build -t "$image:$version" -t "$image:latest" .
+docker build --tag "$image:$arch" --tag "$image:$version-$arch" .
+docker push "$image:$arch"
+docker push "$image:$version-$arch"
 rm -rf dein.vim
-
-docker push "nevillelyh/joplin-cli:$version"
-docker push "nevillelyh/joplin-cli:latest"

e278105f7fde41c63496ec34696f68968650a615
Neville Li <neville.lyh@gmail.com>
2022-05-14 15:04:40 -0400

    [awesome] update audio-defaults.sh

--- .config/awesome/scripts/audio-defaults.sh
+++ .config/awesome/scripts/audio-defaults.sh
@@ -2,8 +2,14 @@
 
 set -euo pipefail
 
-SOURCE=$(pactl list short sources | grep "\<alsa_input\." | grep -i "webcam" | head -n 1 | cut -f 2)
-[[ -n "$SOURCE" ]] && pactl set-default-source "$SOURCE"
+preferred_sources=(webcam c3422we)
+for name in "${preferred_sources[@]}"; do
+    source=$(pactl list short sources | grep "\<alsa_input\." | grep -i "$name" | head -n 1 | cut -f 2 || true)
+    [[ -n "$source" ]] && pactl set-default-source "$source" && break
+done
 
-SINK=$(pactl list short sinks | grep "\<alsa_output\." | grep -i "audioengine" | head -n 1 | cut -f 2)
-[[ -n "$SINK" ]] && pactl set-default-sink "$SINK"
+preferred_sinks=(focusrite audioengine)
+for name in "${preferred_sinks[@]}"; do
+    sink=$(pactl list short sinks | grep "\<alsa_output\." | grep -i "$name" | head -n 1 | cut -f 2 || true)
+    [[ -n "$sink" ]] && pactl set-default-sink "$sink" && break
+done

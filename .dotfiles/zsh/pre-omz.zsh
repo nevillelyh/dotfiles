d8c2ab0baa67baa67d3d5eb4667d6b4c4213b280
Neville Li <neville.lyh@gmail.com>
2022-05-16 09:52:12 -0400

    [zsh] detect login shell correctly

--- .dotfiles/zsh/pre-omz.zsh
+++ .dotfiles/zsh/pre-omz.zsh
@@ -1,22 +1,18 @@
 # Before oh-my-zsh
 
-# Only rebuild PATH in top level interactive shell or tmux (SHLVL > 1)
-# See .zshenv
-if [[ $SHLVL -eq 1 ]] || ([[ -n "$TMUX" ]] && [[ -x /usr/libexec/path_helper ]]) ; then
-    [[ -d /opt/homebrew/bin ]] && export PATH=/opt/homebrew/bin:$PATH
-    [[ -d /opt/homebrew/sbin ]] && export PATH=/opt/homebrew/sbin:$PATH
-    [[ -d $HOME/.local/bin ]] && export PATH=$HOME/.local/bin:$PATH
-    [[ -s $HOME/.cargo/env ]] && source "$HOME/.cargo/env"
+[[ -d /opt/homebrew/bin ]] && export PATH=/opt/homebrew/bin:$PATH
+[[ -d /opt/homebrew/sbin ]] && export PATH=/opt/homebrew/sbin:$PATH
+[[ -d $HOME/.local/bin ]] && export PATH=$HOME/.local/bin:$PATH
+[[ -s $HOME/.cargo/env ]] && source "$HOME/.cargo/env"
 
-    [[ -d /usr/local/go/bin ]] && export PATH=/usr/local/go/bin:$PATH
-    if type go &> /dev/null; then
-        export GOPATH=$HOME/.go
-        export PATH=$HOME/.go/bin:$PATH
-    fi
-
-    # CUDA and cuDNN
-    eval $(nvman env)
+[[ -d /usr/local/go/bin ]] && export PATH=/usr/local/go/bin:$PATH
+if type go &> /dev/null; then
+    export GOPATH=$HOME/.go
+    export PATH=$HOME/.go/bin:$PATH
 fi
+
+# CUDA and cuDNN
+eval $(nvman env)
 
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
--- .zshenv
+++ .zshenv
@@ -1,4 +1,23 @@
 # https://superuser.com/a/583502
-# Mac /etc/zprofile calls path_helper which prepends $PATH
-# Unset it in tmux to rebuild PATH from scratch
-[[ -n "$TMUX" ]] && [[ -x /usr/libexec/path_helper ]] && unset PATH
+#
+# ZSH start up files:
+# - /etc/zsh/zshenv
+# - $HOME/.zshenv
+# - login shell: /etc/zsh/zprofile, $HOME/.zprofile
+# - interactive shell: /etc/zsh/zshrc, $HOME/.zshrc
+#
+# Environment specific:
+# - Linux: /etc/zsh/zshenv sets PATH
+# - Mac: /etc/zprofile calls /usr/libexec/path_helper to prepend PATH
+# - Terminal on Linux: interactive
+# - Terminal on Mac: login & interactive
+# - tmux: login & interactive
+# - SSH: login & interactive
+#
+if [[ -o interactive ]] || [[ -o login ]]; then
+    if [[ -o login ]] && [[ -x /usr/libexec/path_helper ]]; then
+        unset PATH # Let path_helper regenerate
+    else
+        export PATH="/usr/local/bin:/usr/bin:/bin"
+    fi
+fi

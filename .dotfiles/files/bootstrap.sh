2af532e019db924790e48feaabea8188f21acb90
Neville Li <neville.lyh@gmail.com>
2022-05-17 09:22:46 -0400

    [bootstrap] fix msg_box regex

--- .dotfiles/files/bootstrap.sh
+++ .dotfiles/files/bootstrap.sh
@@ -352,9 +352,9 @@ install() {
 }
 
 msg_box() {
-    line="==${1//[[:alnum:][:space:]]/=}=="
+    line="==${1//[[:ascii:]]/=}=="
     echo "$line"
-    echo "= $1 ="
+    echo "| $1 |"
     echo "$line"
 }
 

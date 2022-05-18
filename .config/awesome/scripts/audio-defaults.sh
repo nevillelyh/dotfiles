abdf46f92cbf72e28272b57c4304ce94790bec8d
Neville Li <neville.lyh@gmail.com>
2022-05-18 11:46:46 -0400

    [awesome] add Logitech audio source & sink

--- .config/awesome/scripts/audio-defaults.sh
+++ .config/awesome/scripts/audio-defaults.sh
@@ -2,13 +2,14 @@
 
 # Set default audio source and sink
 
-preferred_sources=(webcam c3422we)
+# C3422WE causes USB error -110
+preferred_sources=(logitech webcam c3422we)
 for name in "${preferred_sources[@]}"; do
     source=$(pactl list short sources | grep "\<alsa_input\." | grep -i "$name" | head -n 1 | cut -f 2)
     [[ -n "$source" ]] && pactl set-default-source "$source" && break
 done
 
-preferred_sinks=(focusrite audioengine)
+preferred_sinks=(focusrite audioengine logitech)
 for name in "${preferred_sinks[@]}"; do
     sink=$(pactl list short sinks | grep "\<alsa_output\." | grep -i "$name" | head -n 1 | cut -f 2)
     [[ -n "$sink" ]] && pactl set-default-sink "$sink" && break

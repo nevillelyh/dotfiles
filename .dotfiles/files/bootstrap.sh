0000b30cf7ba33bc5af08cfc866eba197878ce79
Neville Li <neville.lyh@gmail.com>
2022-05-21 15:43:50 -0400

    [bootstrap] symlink bazelisk to ~/.local/bin

--- .dotfiles/files/bootstrap.sh
+++ .dotfiles/files/bootstrap.sh
@@ -176,8 +176,8 @@ setup_go() {
     go install -v github.com/go-delve/delve/cmd/dlv@latest
     if [[ "$uname_s" == "Linux" ]]; then
         go install github.com/bazelbuild/bazelisk@latest
-        ln -rs "$HOME/.go/bin/bazelisk" "$HOME/.go/bin/bazel"
-        "$HOME/.go/bin/bazel" version
+        ln -s "$HOME/.go/bin/bazelisk" "$HOME/.local/bin/bazel"
+        "$HOME/.local/bin/bazel" version
     fi
 }
 

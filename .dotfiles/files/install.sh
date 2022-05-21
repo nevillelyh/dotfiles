96c917b1ee6521074db34b3b2212e3ed849dab39
Neville Li <neville.lyh@gmail.com>
2022-05-21 14:57:46 -0400

    [bootstrap] add brew in install.sh

--- .dotfiles/files/install.sh
+++ .dotfiles/files/install.sh
@@ -4,6 +4,20 @@
 
 set -euo pipefail
 
+brew_install() {
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        brew install "$@"
+        exit 0
+    fi
+}
+
+brew_install_cask() {
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        brew install --cask "$@"
+        exit 0
+    fi
+}
+
 setup_gpg() {
     url=$1
     gpg=$2
@@ -20,11 +34,13 @@ setup_apt() {
     echo "$repo" | sudo tee "/etc/apt/sources.list.d/$list" > /dev/null
 }
 
-_install_anaconda_mac() {
-    brew install --cask anaconda
-}
+# https://docs.anaconda.com/anaconda/install/linux/
+install_anaconda() {
+    # Do not activate base automatically
+    echo "auto_activate_base: false" > "$HOME/.condarc"
 
-_install_anaconda_linux() {
+    brew_install_cask anaconda
+
     url="https://www.anaconda.com/products/distribution"
     arch=$(uname -m)
     url=$(curl -fsSL $url | grep -o "\<https://repo.anaconda.com/archive/Anaconda3-.*-Linux-$arch.sh\>" | uniq | tail -n 1)
@@ -34,23 +50,9 @@ _install_anaconda_linux() {
     rm "$pkg"
 }
 
-# https://docs.anaconda.com/anaconda/install/linux/
-install_anaconda() {
-    case $(uname -s) in
-        Darwin)
-            _install_anaconda_mac
-            condabin=/opt/homebrew/anaconda3/condabin/conda
-            ;;
-        Linux)
-            _install_anaconda_linux
-            condabin="$HOME/.anaconda3/condabin/conda"
-            ;;
-    esac
-    # Do not activate base automatically
-    "$condabin" config --set auto_activate_base false
-}
+install_awscli() {
+    brew_install awscli
 
-install_aws() {
     arch=$(uname -m)
     curl -fsSL "https://awscli.amazonaws.com/awscli-exe-linux-$arch.zip" -o awscliv2.zip
     unzip awscliv2.zip
@@ -60,6 +62,8 @@ install_aws() {
 
 # Chrome manages its own repository
 install_chrome() {
+    brew_install_cask google-chrome
+
     wget -nv https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
     rm google-chrome-stable_current_amd64.deb
@@ -68,9 +72,10 @@ install_chrome() {
 # Prefer Ubuntu instead
 # https://apt.kitware.com/
 install_cmake() {
+    brew_install cmake
+
     url="https://apt.kitware.com/keys/kitware-archive-latest.asc"
     repo="deb [signed-by=/etc/apt/trusted.gpg.d/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main"
-
     setup_gpg "$url" kitware-archive-keyring.gpg
     setup_apt "$repo" kitware.list
     sudo aptitude update
@@ -79,21 +84,20 @@ install_cmake() {
 
 # https://code.visualstudio.com/docs/setup/linux
 install_code() {
+    brew_install_cask visual-studio-code
+
     url="https://packages.microsoft.com/keys/microsoft.asc"
     repo="deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main"
-
     setup_gpg "$url" packages.microsoft.gpg
     setup_apt "$repo" vscode.list
     sudo aptitude update
     sudo aptitude install -y code
 }
 
-_install_docker_mac() {
-    brew install colima docker docker-compose
-}
-
 # https://docs.docker.com/engine/install/ubuntu/
-_install_docker_linux() {
+install_docker() {
+    brew_install colima docker docker-compose
+
     url="https://download.docker.com/linux/ubuntu/gpg"
     repo="deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     setup_gpg "$url" docker-archive-keyring.gpg
@@ -102,18 +106,10 @@ _install_docker_linux() {
     sudo aptitude install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 }
 
-install_docker() {
-    case $(uname -s) in
-        Darwin)
-            _install_docker_mac
-            ;;
-        Linux)
-            _install_docker_linux
-            ;;
-    esac
-}
 # Dropbox manages its own repository
 install_dropbox() {
+    brew_install_cask dropbox
+
     url="https://linux.dropbox.com/packages/ubuntu/"
     pkg=$(curl -fsSL $url | grep -oP '(?<=href=")[^"]+(?=")' | grep -P "^dropbox_[\d\.]+_amd64.deb$" | tail -n 1)
     wget -nv "$url/$pkg"
@@ -123,9 +119,10 @@ install_dropbox() {
 
 # https://github.com/cli/cli/blob/trunk/docs/install_linux.md
 install_github() {
+    brew_install gh
+
     url="https://cli.github.com/packages/githubcli-archive-keyring.gpg"
     repo="deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main"
-
     setup_gpg "$url" githubcli-archive-keyring.gpg
     setup_apt "$repo" github-cli.list
     sudo aptitude update
@@ -134,6 +131,8 @@ install_github() {
 
 # https://go.dev/doc/install
 install_go() {
+    brew_install go
+
     # TODO: include this in upgrade-dotfiles
     url="https://api.github.com/repos/golang/go/git/refs/tags"
     header="Accept: application/vnd.github.v3+json"
@@ -148,7 +147,6 @@ install_nvidia() {
     url="https://nvidia.github.io/libnvidia-container/gpgkey"
     dist="ubuntu$(lsb_release -rs)"
     repo=$(curl -fsSL "https://nvidia.github.io/libnvidia-container/$dist/libnvidia-container.list" | sed "s#deb https://#deb [signed-by=/etc/apt/trusted.gpg.d/nvidia-container-toolkit-keyring.gpg] https://#g")
-
     setup_gpg "$url" nvidia-container-toolkit-keyring.gpg
     setup_apt "$repo" nvidia-container-toolkit.list
     sudo aptitude update
@@ -167,9 +165,9 @@ install_proton() {
 
 # https://signal.org/download/
 install_signal() {
+    brew_install_cask signal
     url="https://updates.signal.org/desktop/apt/keys.asc"
     repo="deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main"
-
     setup_gpg "$url" signal-desktop-keyring.gpg
     setup_apt "$repo" signal-xenial.list
     sudo aptitude update
@@ -179,9 +177,10 @@ install_signal() {
 # Prefer snap instead
 # https://packagecloud.io/app/slacktechnologies/slack/gpg#gpg-apt
 install_slack() {
+    brew_install_cask slack
+
     url="https://packagecloud.io/slacktechnologies/slack/gpgkey"
     repo=$(curl -fsSL "https://packagecloud.io/install/repositories/slacktechnologies/slack/config_file.list?os=debian&dist=jessie&source=script")
-
     setup_gpg "$url" slacktechnologies_slack.gpg
     setup_apt "$repo" slacktechnologies_slack.list
     sudo aptitude update
@@ -191,9 +190,10 @@ install_slack() {
 # Prefer snap instead
 # https://www.spotify.com/us/download/linux/
 install_spotify() {
+    brew_install_cask spotify
+
     url="https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg"
     repo="deb http://repository.spotify.com stable non-free"
-
     setup_gpg "$url" spotify.gpg
     setup_apt "$repo" spotify.list
     sudo aptitude update
@@ -202,12 +202,12 @@ install_spotify() {
 
 # https://repo.steampowered.com/steam/
 install_steam() {
+    brew_install_cask steam
+
     url="https://repo.steampowered.com/steam/archive/stable/steam.gpg"
     line1="deb [arch=amd64,i386] http://repo.steampowered.com/steam/ stable steam"
     line2="deb-src [arch=amd64,i386] http://repo.steampowered.com/steam/ stable steam"
     repo=$(echo -e "$line1\n$line2")
-    echo "$repo"
-
     setup_gpg "$url" steam.gpg
     setup_apt "$repo" steam.list
     sudo aptitude update
@@ -216,9 +216,10 @@ install_steam() {
 
 # https://www.sublimetext.com/docs/linux_repositories.html
 install_sublime() {
+    brew_install_cask sublime-text
+
     url="https://download.sublimetext.com/sublimehq-pub.gpg"
     repo="deb https://download.sublimetext.com/ apt/stable/"
-
     setup_gpg "$url" sublimehq-pub.gpg
     setup_apt "$repo" sublime-text.list
     sudo aptitude update
@@ -227,6 +228,8 @@ install_sublime() {
 
 # https://aka.ms/get-teams-linux
 install_teams() {
+    brew_install_cask microsoft-teams
+
     url="https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/"
     pkg=$(curl -fsSL $url | grep -oP '(?<=href=")[^"]+(?=")' | grep -P "^teams_[\d\.]+_amd64.deb$" | tail -n 1)
     wget -nv "$url/$pkg"
@@ -237,9 +240,10 @@ install_teams() {
 
 # https://www.vaultproject.io/downloads
 install_vault() {
+    brew_install vault
+
     url="https://apt.releases.hashicorp.com/gpg"
     repo="deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-
     setup_gpg "$url" hashicorp.gpg
     setup_apt "$repo" hashicorp.list
     sudo aptitude update

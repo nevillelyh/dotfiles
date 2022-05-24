2f1eeaa85533f22ba1a9bb1e5cce7ea1850dc046
Neville Li <neville.lyh@gmail.com>
2022-05-24 15:20:56 -0400

    [awesome] replace Thunderbird with Evolution

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -985,10 +985,10 @@ awful.rules.rules = {
     { rule_any = { name = { "JetBrains Toolbox" },
         }, properties = { placement = awful.placement.top_right }
     },
-    { rule_any = { class = { "Microsoft Teams - Preview" },
+    { rule_any = { class = { "Evolution" },
       }, properties = { tag = awful.screen.focused().tags[8] }
     },
-    { rule_any = { class = { "Thunderbird" },
+    { rule_any = { class = { "Microsoft Teams - Preview" },
       }, properties = { tag = awful.screen.focused().tags[8] }
     },
     { rule_any = { class = { "Steam" },

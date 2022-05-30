a55d4b06aff30002e99058d1d5d7cf30a524688b
Neville Li <neville.lyh@gmail.com>
2022-05-30 00:44:41 -0400

    [zsh] fix virtualenvwrapper.sh path

--- .dotfiles/zsh/post-omz.zsh
+++ .dotfiles/zsh/post-omz.zsh
@@ -35,7 +35,7 @@ export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,hl:#bd93f9 --color=fg+:#f8f8f2,hl+:#
 if type virtualenvwrapper.sh &> /dev/null; then
     export VIRTUALENVWRAPPER_PYTHON=$(which python3)
     export WORKON_HOME=$HOME/.virtualenvs
-    source "$HOME/.local/bin/virtualenvwrapper.sh"
+    source $(which virtualenvwrapper.sh)
 fi
 
 # Using system Python by default, base is not activated

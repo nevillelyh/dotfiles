0054ce75f60969cdd45c074b9e4ece4e0d501f8e
Neville Li <neville.lyh@gmail.com>
2022-06-25 19:49:32 -0400

    [misc] cross-build joplin-cli

--- .dotfiles/files/joplin-cli/build-manifest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Build and publish cross-platform manifest
-
-docker manifest create nevillelyh/joplin-cli:latest nevillelyh/joplin-cli:arm64 nevillelyh/joplin-cli:amd64
-docker manifest push nevillelyh/joplin-cli:latest
--- .dotfiles/files/joplin-cli/build.sh
+++ .dotfiles/files/joplin-cli/build.sh
@@ -6,19 +6,12 @@ set -euo pipefail
 
 image=nevillelyh/joplin-cli
 version=$(grep "npm install" Dockerfile | grep -o "\<joplin@[0-9\.]\+$" | cut -d "@" -f 2)
-arch=$(uname -m)
-case $arch in
-    aarch64)
-        arch=arm64
-        ;;
-    x86_64)
-        arch=amd64
-        ;;
-esac
 
 [[ -d dein.vim ]] && rm -rf dein.vim
 git clone https://github.com/Shougo/dein.vim.git
-docker build --tag "$image:$arch" --tag "$image:$version-$arch" .
-docker push "$image:$arch"
-docker push "$image:$version-$arch"
+
+# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx inspect cross-build &> /dev/null || docker buildx create --name cross-build --use
+docker buildx build --platform linux/amd64,linux/arm64 --tag "$image:latest" --tag "$image:$version" --push .
+
 rm -rf dein.vim

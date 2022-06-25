eef4adafbdfba4760e3b5de5500f9c7c44269b4e
Neville Li <neville.lyh@gmail.com>
2022-06-25 17:00:57 -0400

    [misc] add presto-cli and trino-cli

---
+++ .local/bin/bin-wrapper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -euo pipefail
+
+links() {
+    url=$1
+    curl -fsSL "$url" | grep -o 'href="[^"]\+"' | sed 's/href="\([^"]*\)"/\1/'
+}
+
+download() {
+    (( ttl = 7 * 24 * 60 * 60 ))
+    (( age = ttl + 1 ))
+    [[ -f "$file" ]] && age=$(echo "$(date "+%s")" - "$(date -r "$file" "+%s")" | bc -l)
+    if [[ $age -ge $ttl ]]; then
+        mkdir -p "$(dirname "$file")"
+        curl -fsSL "$(latest)" -o "$file"
+        [[ "$exe" -eq 1 ]] && chmod +x "$file"
+    fi
+}
+
+run_presto-cli() {
+    latest() {
+        js_url=https://prestodb.io/static/js/version.js
+        version="$(curl -fsSL "$js_url" | grep "\<presto_latest_presto_version\>" | sed "s/[^']*'\([^']*\)';/\1/")"
+        echo "https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/$version/presto-cli-$version-executable.jar"
+    }
+    file="$HOME/.local/libexec/presto-cli"
+    exe=1
+    download
+    "$file" "$@"
+}
+
+run_trino-cli() {
+    latest() {
+        links "https://trino.io/download.html" | grep "trino-cli-[0-9]\+-executable.jar"
+    }
+    file="$HOME/.local/libexec/trino-cli"
+    exe=1
+    download
+    "$file" "$@"
+}
+
+bin="$(basename "$0")"
+"run_$bin" "$@"

252403abe6db0ec932b1fd3026caa6c7430e8d14
Neville Li <neville.lyh@gmail.com>
2022-08-09 19:09:21 -0400

    [git] remove --fill from gh pr create alias

--- .dotfiles/zsh/post-omz.zsh
+++ .dotfiles/zsh/post-omz.zsh
@@ -12,7 +12,7 @@ esac
 alias cat='bat'
 alias diff='colordiff'
 alias digc='docker images --quiet --filter dangling=true | xargs -r docker rmi'
-alias ghpr='gh pr create --fill'
+alias ghpr='gh pr create'
 alias l='exa -la' # 'ls -lah'
 alias la='exa -la' # 'ls -lAh'
 alias ll='exa -l' # 'ls -lh'

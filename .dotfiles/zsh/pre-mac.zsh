ef7a92ab31b2fd61070cb428cd26a2a86d97036d
Neville Li <neville.lyh@gmail.com>
2022-10-25 13:39:13 -0400

    [zsh] load brew completions last

--- .dotfiles/zsh/pre-mac.zsh
+++ .dotfiles/zsh/pre-mac.zsh
@@ -4,4 +4,4 @@ export PATH="$HOME/Library/Application Support/JetBrains/Toolbox/scripts":$PATH
 [[ -d /opt/homebrew/sbin ]] && export PATH=/opt/homebrew/sbin:$PATH
 
 # https://docs.brew.sh/Shell-Completion
-FPATH="$(brew --prefix)/share/zsh/site-functions:$FPATH"
+FPATH="$FPATH:$(brew --prefix)/share/zsh/site-functions"

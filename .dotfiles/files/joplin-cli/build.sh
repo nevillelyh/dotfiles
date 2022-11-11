fbccd8bfab10d74348f16192573ef8b336791b9f
Neville Li <neville.lyh@gmail.com>
2022-11-11 17:41:18 -0500

    [misc] reset qemu-user-static in joplin-cli

--- .dotfiles/files/joplin-cli/build.sh
+++ .dotfiles/files/joplin-cli/build.sh
@@ -10,7 +10,7 @@ version=$(grep "npm install" Dockerfile | grep -o "\<joplin@[0-9\.]\+$" | cut -d
 [[ -d dein.vim ]] && rm -rf dein.vim
 git clone https://github.com/Shougo/dein.vim.git
 
-# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx inspect cross-build &> /dev/null || docker buildx create --name cross-build --use
 docker buildx build --platform linux/amd64,linux/arm64 --tag "$image:latest" --tag "$image:$version" --push .
 

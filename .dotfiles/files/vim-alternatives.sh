d20332f1304d5f7a0b47ad5ae6ded4b253bcee91
Neville Li <neville.lyh@gmail.com>
2022-12-20 03:49:22 +0000

    [misc] install nvim alternatives if not registered

--- .dotfiles/files/vim-alternatives.sh
+++ .dotfiles/files/vim-alternatives.sh
@@ -3,5 +3,8 @@
 # Set NeoVim as default on Ubuntu
 
 for name in $(update-alternatives --get-selections | grep vim | awk '{print $1}'); do
+    if ! update-alternatives --list "$name" | grep -q "/usr/bin/nvim"; then
+        sudo update-alternatives --install "/usr/bin/$name" "$name" /usr/bin/nvim 100
+    fi
     sudo update-alternatives --config "$name"
 done

262830e6d32ca41d0bf74a24e3171b0af79debdf
Neville Li <neville.lyh@gmail.com>
2022-12-28 13:25:46 -0500

    [vim] rework vim-alternatives.sh

--- .dotfiles/files/vim-alternatives.sh
+++ .dotfiles/files/vim-alternatives.sh
@@ -3,8 +3,18 @@
 # Set NeoVim as default on Ubuntu
 
 for name in $(update-alternatives --get-selections | grep vim | awk '{print $1}'); do
-    if ! update-alternatives --list "$name" | grep -q "/usr/bin/nvim"; then
-        sudo update-alternatives --install "/usr/bin/$name" "$name" /usr/bin/nvim 100
-    fi
-    sudo update-alternatives --config "$name"
+    readarray -t paths < <(update-alternatives --list "$name" | grep '^/usr/\(bin/nvim\|libexec/neovim[^/]*/\)')
+    case "${#paths[@]}" in
+        0) echo "No alternatives for $name" ;;
+        1)
+            echo "Use ${paths[0]} for $name"
+            sudo update-alternatives --set "$name" "${paths[0]}"
+            ;;
+        *)
+            echo "${#paths[@]} alternatives for $name:"
+            for path in "${paths[@]}"; do
+                echo "    $path"
+            done
+            ;;
+    esac
 done

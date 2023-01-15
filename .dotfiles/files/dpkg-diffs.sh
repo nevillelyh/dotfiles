d39b075e1cd1fcf784e866f011bdde37422ffb1f
Neville Li <neville.lyh@gmail.com>
2023-01-15 14:42:53 -0500

    [misc] add dpkg-diffs.sh

---
+++ .dotfiles/files/dpkg-diffs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Check changes to local files
+
+set -euo pipefail
+
+url="https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/dpkg-diffs/dpkg-diffs-0.1.0.tar.gz"
+
+tmp="$(mktemp -d -t dpkg-diff.XXXXXXXXXX)"
+trap 'rm -rf $tmp' EXIT
+curl -fsSL "$url" | tar -C "$tmp" -xz
+diffs="$(find "$tmp" -type f -name dpkg-diffs | tail -n 1)"
+
+get_packages() {
+    echo >&2 "# sudo dpkg -V"
+    dpkgv="$(sudo dpkg -V || true)"
+    readarray -t files < <(echo "$dpkgv" | cut -c 13-)
+    for file in "${files[@]}"; do
+        readarray -t pkgs < <(dpkg-query -S "$file" | cut -d ':' -f 1 | sed 's/, /\n/g')
+        for pkg in "${pkgs[@]}"; do
+            echo >&2 "# $pkg: $file"
+            echo "$pkg"
+        done
+    done | sort -u
+}
+
+readarray -t pkgs < <(get_packages)
+for pkg in "${pkgs[@]}"; do
+    "$diffs" -d "$pkg"
+done

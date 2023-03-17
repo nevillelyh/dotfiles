382f772f77c0a800d7eb1d54c2df6350764d80ef
Neville Li <neville.lyh@gmail.com>
2023-03-17 16:17:14 -0400

    [zsh] improve bs.sh

--- .dotfiles/files/bs.sh
+++ .dotfiles/files/bs.sh
@@ -147,12 +147,114 @@ bs_timestamp() {
 
 _bs_test_exec() {
     _bs_test_stdout "pong" bs_df files/bs-test.sh ping
+
+    local file
+    file=$(bs_temp_file bs-test)
+    echo "foobar" > "$file"
+    bs_sed_i 's/bar/baz/g' "$file"
+    _bs_test_stdout "foobaz" cat "$file"
+    rm "$file"
+
     _bs_test_stdout "20230101000000" bs_timestamp -r 1672531200 -u
+}
+
+############################################################
+# Resources
+############################################################
+
+bs_file_age() {
+    local file=$1
+    echo "$(date "+%s")" - "$(date -r "$file" "+%s")" | bc -l
+}
+
+bs_urls() {
+    local url=$1
+    curl -fsSL "$url" | grep -o 'href="[^"]\+"' | sed 's/href="\([^"]*\)"/\1/'
+}
+
+bs_gh_latest() {
+    local repo=$1
+    local url="https://api.github.com/repos/$repo/releases/latest"
+    local header="Accept: application/vnd.github.v3+json"
+    curl -fsSL -H "$header" "$url" | jq --raw-output '.tag_name' | sed 's/^v//g'
+}
+
+bs_temp_file() {
+    local prefix=$1
+    local path
+    case "$BS_UNAME_S" in
+        Darwin) path=$(mktemp -t "$prefix") ;;
+        Linux)  path=$(mktemp -t "$prefix.XXXXXXXX") ;;
+    esac
+    echo "$path"
+}
+
+bs_temp_dir() {
+    local prefix=$1
+    local path
+    case "$BS_UNAME_S" in
+        Darwin) path=$(mktemp -d -t "$prefix") ;;
+        Linux)  path=$(mktemp -d -t "$prefix.XXXXXXXX") ;;
+    esac
+    echo "$path"
+}
+
+_bs_test_resources() {
+    local path
+    local base
+
+    path="$(bs_temp_file bs-test)"
+    sleep 1
+    _bs_test_stdout "1" bs_file_age "$path"
+    rm "$path"
+
+    local url
+    url=$(bs_urls https://github.com/nevillelyh | grep -c '^https://github.com$' || true)
+    if [[ $url -eq 0 ]]; then
+        _bs_test_fail "bs_urls https://github.com/nevillelyh"
+    else
+        _bs_test_pass "bs_urls https://github.com/nevillelyh"
+    fi
+
+    _bs_test_stdout "0.2.0" bs_gh_latest nevillelyh/shapeless-datatype
+
+    path="$(bs_temp_file bs-test)"
+    base="$(basename "$path")"
+
+    if [[ -f "$path" ]] && [[ "$path" = /* ]] && [[ "$base" = bs-test.* ]]; then
+        _bs_test_pass "bs_temp_file $path"
+    else
+        _bs_test_fail "bs_temp_file $path"
+    fi
+    rm "$path"
+
+    path="$(bs_temp_dir bs-test)"
+    base="$(basename "$path")"
+    if [[ -d "$path" ]] && [[ "$path" = /* ]] && [[ "$base" = bs-test.* ]]; then
+        _bs_test_pass "bs_temp_dir $path"
+    else
+        _bs_test_fail "bs_temp_dir $path"
+    fi
+    rm -rf "$path"
+
 }
 
 ############################################################
 # Tests
 ############################################################
+
+_bs_test_passes=0
+_bs_test_failures=0
+
+_bs_test_fail() {
+    (( _bs_test_failures++ ))
+    bs_error "[FAIL] $*"
+}
+
+_bs_test_pass() {
+    (( _bs_test_passes++ ))
+    bs_success "[PASS] $*"
+}
 
 _bs_test_stdout() {
     local expected="$1"
@@ -160,9 +262,9 @@ _bs_test_stdout() {
     local actual
     actual="$("$@")"
     if [[ "$expected" == "$actual" ]]; then
-        bs_success "[PASS] $*"
+        _bs_test_pass "$@"
     else
-        bs_error "[FAIL] $*"
+        _bs_test_fail "$@"
         echo "Test failure, expected: $expected, actual: $actual"
     fi
 }
@@ -171,4 +273,11 @@ _bs_test() {
     _bs_test_ansi
     _bs_test_array
     _bs_test_exec
+    _bs_test_resources
+    (( _bs_test_total = _bs_test_passes + _bs_test_failures ))
+    if [[ $_bs_test_failures -eq 0 ]]; then
+        bs_success "All $_bs_test_total tests passed"
+    else
+        bs_fatal "$_bs_test_failures out of $_bs_test_total tests failed"
+    fi
 }

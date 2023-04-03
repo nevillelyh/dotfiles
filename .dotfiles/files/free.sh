475a28695da51e3fe57de737d254ea4bd18e8f0d
Neville Li <neville.lyh@gmail.com>
2023-04-03 15:24:39 +0000

    [misc] handle xargs empty list in free.sh

--- .dotfiles/files/free.sh
+++ .dotfiles/files/free.sh
@@ -14,7 +14,7 @@ fi
 find_delete() {
     local dir=$1
     local size
-    size="$(find "$@" -print0 | xargs -0 du -hcs | tail -n 1 | awk '{print $1}')"
+    size="$(find "$@" -print0 | xargs -0 -r du -hcs | tail -n 1 | awk '{print $1}')"
     [[ -z "$size" ]] && size="0B"
     printf "%s\t%s\n" "$size" "$dir"
     find "$@" -delete

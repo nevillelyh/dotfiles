40de95ed9f9ea5dc501fb4567ccfddfe64254f14
Neville Li <neville.lyh@gmail.com>
2023-06-29 19:49:15 -0400

    [bootstrap] add docker and tailscale cask

--- .dotfiles/files/bootstrap.sh
+++ .dotfiles/files/bootstrap.sh
@@ -18,7 +18,7 @@ fi
 # pinentry-mac - for GPG
 # App Store - AdGuard for Safari, Instapaper, Kindle, Messenger, Slack, The Unarchiver, WhatsApp
 BREWS=(bat btop cmake colordiff dust exa fd fzf git git-delta gitui golang gpg htop jq mas neovim ninja pinentry-mac python ripgrep shellcheck tmux wget zoxide)
-CASKS=(alacritty alfred dbeaver-community discord dropbox expressvpn github iterm2 jetbrains-toolbox joplin sublime-text visual-studio-code vimr zotero)
+CASKS=(alacritty alfred dbeaver-community discord docker dropbox expressvpn github iterm2 jetbrains-toolbox joplin sublime-text tailscale visual-studio-code vimr zotero)
 CASKS_OPT=(adobe-creative-cloud anki firefox google-chrome google-cloud-sdk guitar-pro hiarcs-chess-explorer macdive microsoft-edge signal shearwater-cloud spotify steam subsurface transmission vlc)
 # AdGuard Bitwarden Xcode Unarchiver Kindle Messenger Pocket Slack WhatsApp
 MAS=(1440147259 1352778147 425424353 405399194 1480068668 1477385213 803453959 1147396723 497799835)

59f76ac522064a4b4ba144710f422cb8fb45c9d4
Neville Li <neville.lyh@gmail.com>
2023-07-15 11:37:47 -0400

    [misc] simplify alacritty build

--- .dotfiles/builds/alacritty/build.sh
+++ .dotfiles/builds/alacritty/build.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
+# Build Alacritty for arm64
+
 set -euo pipefail
 
-version="v0.12.2"
+version="0.12.2"
 icon_url='https://raw.githubusercontent.com/alacritty/alacritty/master/extra/logo/alacritty-term.svg'
+
+read -r -d '' desktop << EOF || true
+[Desktop Entry]
+Version=$version
+Type=Application
+Name=Alacritty
+Exec=/home/neville/.dotfiles/libexec/cache/alacritty
+Icon=alacritty.svg
+EOF
 
 run_guest() {
     git clone https://github.com/alacritty/alacritty.git
     cd /alacritty
-    git checkout "$version"
+    git checkout "v$version"
     make binary
 }
 
 run_host() {
-    base="$(readlink -f "$0")"
-    cd "$base"
-    docker run --name alacritty --volume "$base":/build.sh rust:bookworm /build.sh
+    script="$(readlink -f "$0")"
+    cd "$(dirname "$script")"
+    docker run --name alacritty --volume "$script":/build.sh rust:bookworm /build.sh
     docker cp alacritty:/alacritty/target/release/alacritty .
     docker rm alacritty
 
@@ -23,7 +34,7 @@ run_host() {
     mkdir -p "$HOME/.local/share/icons"
     curl -fsSL "$icon_url" -o "$HOME/.local/share/icons/alacritty.svg"
     mkdir -p "$HOME/.local/share/applications"
-    ln -frs alacritty.desktop "$HOME/.local/share/applications"
+    echo "$desktop" > "$HOME/.local/share/applications/alacritty.desktop"
 }
 
 if [[ -f /.dockerenv ]]; then

8883457836f39dbb74575a146b9386e01feb300e
Neville Li <neville.lyh@gmail.com>
2024-01-23 20:38:06 -0500

    [awesome] update audio defaults

--- .config/awesome/scripts/audio-defaults.sh
+++ .config/awesome/scripts/audio-defaults.sh
@@ -2,14 +2,13 @@
 
 # Set default audio source and sink
 
-# C3422WE causes USB error -110
-preferred_sources=(webcam nt-usb logitech)
+preferred_sources=(webcam)
 for name in "${preferred_sources[@]}"; do
     source=$(pactl list short sources | grep '\<alsa_input\.' | grep -i "$name" | head -n 1 | cut -f 2)
     [[ -n "$source" ]] && pactl set-default-source "$source" && break
 done
 
-preferred_sinks=(audioengine focusrite logitech)
+preferred_sinks=(focusrite)
 for name in "${preferred_sinks[@]}"; do
     sink=$(pactl list short sinks | grep '\<alsa_output\.' | grep -i "$name" | head -n 1 | cut -f 2)
     [[ -n "$sink" ]] && pactl set-default-sink "$sink" && break

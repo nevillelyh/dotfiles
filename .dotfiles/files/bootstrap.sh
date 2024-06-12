41e598532fe63ad10ede1be88e6743c425d38602
Neville Li <neville.lyh@gmail.com>
2024-06-12 00:19:34 -0400

    [misc] replace exa with eza on linux

--- .dotfiles/files/bootstrap.sh
+++ .dotfiles/files/bootstrap.sh
@@ -33,9 +33,9 @@ MAS=(1440147259 1352778147 405399194 441258766 1480068668 1477385213 803453959 4
 # libfuse2 - for AppImage, e.g. JetBrains Toolbox, NeoVim
 # unzip, zip - for SDKMAN
 # Not available or outdated in Ubuntu - bat, git-delta, zoxide
-DEB_PKGS=(build-essential colordiff exa fd-find fzf htop jq libfuse2 ninja-build python3-venv ripgrep shellcheck tmux unzip zip zsh)
+DEB_PKGS=(build-essential colordiff fd-find fzf htop jq libfuse2 ninja-build python3-venv ripgrep shellcheck tmux unzip zip zsh)
 DEB_GUI_PKGS=(alacritty awesome compton fonts-powerline gnome-screensaver gnome-screenshot ubuntu-restricted-extras vlc wmctrl xautolock xcalib xprintidle)
-LINUX_CRATES=(bat du-dust git-delta gitui zoxide)
+LINUX_CRATES=(bat du-dust eza git-delta gitui zoxide)
 
 # PIP packages:
 PIP_PKGS=(flake8 ipython requests virtualenv virtualenvwrapper)

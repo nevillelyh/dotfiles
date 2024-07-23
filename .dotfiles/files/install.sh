36182c3d83babc0216ff044478028feac75e9981
Neville Li <neville.lyh@gmail.com>
2024-07-23 18:01:38 -0400

    [misc] pin dropbox for ubuntu 22.04 compatibility

--- .dotfiles/files/install.sh
+++ .dotfiles/files/install.sh
@@ -195,7 +195,7 @@ cmd_dropbox() {
     local url
     url="https://linux.dropbox.com/packages/$(distro)/"
     local pkg
-    pkg=$(curl -fsSL "$url" | grep -oP '(?<=href=")[^"]+(?=")' | grep -P '^dropbox_[\d\.]+_amd64.deb$' | tail -n 1)
+    pkg=$(curl -fsSL "$url" | grep -oP '(?<=href=")[^"]+(?=")' | grep -P '^dropbox_[\d\.]+_amd64.deb$' | grep '2020\.03\.04' | tail -n 1)
     wget -nv "$url/$pkg"
     sudo dpkg -i dropbox_*_amd64.deb
     sudo apt-get install -fy

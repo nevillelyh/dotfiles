9567369da78e0114fadd0a2f27527d7d4d800526
Neville Li <neville.lyh@gmail.com>
2024-07-23 22:34:28 -0400

    [zsh] remove obsolete btop reference

--- .dotfiles/zsh/post-linux.zsh
+++ .dotfiles/zsh/post-linux.zsh
@@ -1,10 +1,3 @@
-# Workaround for themes relative path
-# alias btop='/snap/btop/current/usr/local/bin/btop'
-
-# Compile from source for GPU support
-# $HOME/.local/bin/btop
-# https://github.com/aristocratos/btop?tab=readme-ov-file#compilation-linux
-
 alias open='xdg-open'
 alias xc='xclip -selection clipboard'
 alias dgrun='docker run --gpus all --privileged'

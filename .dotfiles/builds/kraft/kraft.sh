89a00673367798a4354bf5a0206be9398b8ad472
Neville Li <neville.lyh@gmail.com>
2024-08-03 17:55:42 -0400

    [misc] delete obsolete builds and commands

--- .dotfiles/builds/kraft/kraft.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-bin/kafka-storage format \
-    --config etc/kafka/kraft/server.properties \
-    --cluster-id "$(bin/kafka-storage random-uuid)"
-bin/kafka-server-start etc/kafka/kraft/server.properties "$@"

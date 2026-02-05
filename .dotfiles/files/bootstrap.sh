7a47adff4fe5b2ddd2d158238107d69c25dc597e
Neville Li <neville.lyh@gmail.com>
2026-02-04 19:07:22 -0600

    [misc] add claude & codex to bootstrap.sh

--- .dotfiles/files/bootstrap.sh
+++ .dotfiles/files/bootstrap.sh
@@ -20,7 +20,7 @@ GUI=${GUI:-1}
 # pinentry-mac - for GPG
 # App Store - AdGuard for Safari, Instapaper, Kindle, Messenger, Slack, The Unarchiver, WhatsApp
 BREWS=(bat btop cmake colordiff dust eza fd git git-delta gitui golang gpg htop jq mas neovim ninja pinentry-mac ripgrep shellcheck tmux wget zoxide)
-CASKS=(alacritty alfred dbeaver-community discord docker dropbox expressvpn github iterm2 jetbrains-toolbox notion scroll-reverser sublime-text tailscale visual-studio-code vimr zotero)
+CASKS=(alacritty alfred claude claude-code codex codex-app dbeaver-community discord docker dropbox expressvpn github iterm2 jetbrains-toolbox notion scroll-reverser sublime-text tailscale visual-studio-code vimr zotero)
 CASKS_OPT=(adobe-creative-cloud anki firefox google-chrome google-cloud-sdk guitar-pro hiarcs-chess-explorer macdive microsoft-edge retroarch signal shearwater-cloud spotify steam subsurface transmission vlc waves-central)
 # AdGuard Bitwarden Kindle Magnet Messenger Pocket Slack Unarchiver WhatsApp
 MAS=(1440147259 1352778147 472772 441258766 1480068668 1477385213 803453959 425424353 310633997)

d36e2b532458487b35f438d0a16757ef94af5b7c
Neville Li <neville.lyh@gmail.com>
2026-03-18 09:37:57 -0400

    [misc] narrow apt arch for antigravity

--- .dotfiles/files/install.sh
+++ .dotfiles/files/install.sh
@@ -106,7 +106,8 @@ cmd_antigravity() {
     brew_install_cask antigravity || return 0
 
     local url="https://us-central1-apt.pkg.dev/doc/repo-signing-key.gpg"
-    local repo="deb [signed-by=/etc/apt/trusted.gpg.d/antigravity.gpg] https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/ antigravity-debian main"
+    local repo
+    repo="deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/antigravity.gpg] https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/ antigravity-debian main"
     setup_gpg "$url" antigravity.gpg
     setup_apt "$repo" antigravity.list
     sudo apt-get update

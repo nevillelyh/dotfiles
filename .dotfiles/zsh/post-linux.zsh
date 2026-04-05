f87dedcd3684d34232a270e070171d4a901777f4
Neville Li <neville.lyh@gmail.com>
2026-04-04 21:16:13 -0400

    [zsh] use system mount/umount on linux

--- .dotfiles/zsh/post-linux.zsh
+++ .dotfiles/zsh/post-linux.zsh
@@ -1,6 +1,16 @@
 alias open='xdg-open'
 alias dgrun='docker run --gpus all --privileged'
 
+# Linux Homebrew installs these commands without setuid, override them
+# find /usr/bin -maxdepth 1 -perm -4000 -printf '%f\n' 2>/dev/null |
+# while IFS= read -r b; do
+#   if [[ -L "/home/linuxbrew/.linuxbrew/bin/$b" ]]; then
+#     echo "alias $b=/usr/bin/$b"
+#   fi
+# done
+alias mount=/usr/bin/mount
+alias umount=/usr/bin/umount
+
 # copy/paste
 xc() {
     if [[ -t 0 ]]; then

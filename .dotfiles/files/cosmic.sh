0a6953d9f66a504c9d2154b55dacfb5a3318bf80
Neville Li <neville.lyh@gmail.com>
2026-04-19 12:22:28 -0400

    [misc] copy dropbox icons in cosmic.sh

--- .dotfiles/files/cosmic.sh
+++ .dotfiles/files/cosmic.sh
@@ -9,7 +9,7 @@ v="$(cat "$HOME/.dropbox-dist/VERSION")"
 d="$HOME/.local/share/pixmaps"
 mkdir -p "$d"
 for f in "$HOME/.dropbox-dist/dropbox-lnx.$m-$v/images/hicolor/16x16/status/"*.png; do
-  ln -sf "$f" "$d"
+  cp "$f" "$d"
 done
 
 d="$HOME/.local/share/icons/hicolor/32x32/apps"

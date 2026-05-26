659ca6cc5a83ba9821c9bd77a1dbc3c6a3902741
Neville Li <neville.lyh@gmail.com>
2026-05-26 18:27:33 -0400

    [misc] add krew to bootstrap

--- .dotfiles/files/bootstrap.sh
+++ .dotfiles/files/bootstrap.sh
@@ -48,6 +48,7 @@ BREWS=(
     k9s
     kconf
     kind
+    krew
     kubectl
     kubectx
     kustomize
@@ -381,6 +382,15 @@ cmd_jvm() {
     touch "$HOME/.bootstrap-jvm"
 }
 
+cmd_kubectl() {
+    [[ -f "$HOME/.bootstrap-kubectl" ]] && return 0
+    bs_info_box "Setting up kubectl"
+
+    kubectl krew install cert-manager cnpg tree stern
+
+    touch "$HOME/.bootstrap-kubectl"
+}
+
 cmd_code() {
     [[ -f "$HOME/.bootstrap-code" ]] && return 0
     bs_info_box "Setting up Visual Studio Code"
@@ -476,6 +486,7 @@ bootstrap() {
     cmd_fnm
     cmd_go
     cmd_jvm
+    cmd_kubectl
     cmd_code
     cmd_fonts
     cmd_zsh
